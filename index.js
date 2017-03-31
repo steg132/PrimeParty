001178d1a80dd89512087d75db26925754db16ae
Ryan Schumacher <Ryan.Schumacher@possible.com>
2017-03-30 23:11:29 -0400

    adding new ask

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ var Alexa = require('alexa-sdk');
 var APP_ID = "amzn1.ask.skill.b2f22d3f-993e-42bc-9651-ea88ce3d4ee3"; //OPTIONAL: replace with "amzn1.echo-sdk-ams.app.[your-unique-value-here]";
 var SKILL_NAME = 'Prime Party';
 
-
-console.log("fail") 
 exports.handler = function(event, context, callback) {
     var alexa = Alexa.handler(event, context);
     alexa.APP_ID = APP_ID;
@@ -17,10 +15,13 @@ exports.handler = function(event, context, callback) {
 var handlers = {
     'LaunchRequest': function () {
     	var speechOutput = "Welcome to Prime Party. What kind of party would you like to plan?";
+    	this.emit(":ask", speechOutput);
 
     },
     'PlanPartyIntent': function () {
-    	var partyType = this.event.request.intent.slots.party
-        var speechOutput = "Congratulations! You have planned a partyType";
+    	var partyType = this.event.request.intent.slots.party.value
+        
+        var speechOutput = "Congratulations! You have planned a " + partyType;
+    	this.emit(":tell", speechOutput);
     }
 };
